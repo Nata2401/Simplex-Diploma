95040d0784fd8014a3da3845bad9a4af8d61bbd1
Nata2401 <kuzyan.rd@yandex.ru>
2022-01-16 21:35:52 +0300

    Реализован плавный скролл при клике на стрелочку вверх

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -22,5 +22,23 @@ const menu = () => {
       });
     }
   });
+
+  const arrowUp = document.querySelector(".up"),
+    servicesSection = document.querySelector(".services-section"),
+    sectionTop = servicesSection.offsetTop;
+
+  arrowUp.style.display = "none";
+
+  window.onscroll = function () {
+    if (window.pageYOffset >= sectionTop) {
+      arrowUp.style.display = "block";
+    } else {
+      arrowUp.style.display = "none";
+    }
+  };
+
+  arrowUp.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
 };
 export default menu;
