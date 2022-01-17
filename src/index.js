8ad0d10ff95e7610b627fb5281e003b937f78bb0
Nata2401 <kuzyan.rd@yandex.ru>
2022-01-17 18:20:05 +0300

    Реализован аккордеон в блоке ЧАВО

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import modal from "./modules/modal";
 import slider from "./modules/slider";
 import menu from "./modules/menu";
 import carousel from "./modules/carousel";
+import accordion from "./modules/accordion";
 
 modal();
 slider();
 menu();
 carousel();
+accordion();
 
